e5cb4dd8537da935ad8a00def7cadea8af0b5d80
Sergii Karpus <karpus.sergey@gmail.com>
2022-10-30 01:42:30 +0300

    In Progress Task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const divGalleryRef = document.querySelector(".gallery");
+//Створення розмітки Галереї
+const createGallary = (items) => {
+   return items
+      .map((item) =>
+         `<li><a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+         </a></li>`
+      )
+      .join("");
+};
+
+divGalleryRef.innerHTML = createGallary(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.on('shown.simplelightbox', function () {
+   captionsData();
+});
