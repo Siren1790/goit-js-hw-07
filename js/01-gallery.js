e54dd9378fe608c5943f235e5025771f38531f95
Sergii Karpus <karpus.sergey@gmail.com>
2022-10-29 23:42:24 +0300

    Done Task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,5 @@
 import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from 'basiclightbox';
 // Change code below this line
 
 // 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
@@ -6,5 +7,46 @@ import { galleryItems } from './gallery-items.js';
 // 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
 // 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
 // 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+const divGalleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGallary = (items) => {
+   return items
+      .map((item) => 
+         `<div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+               <img
+                  class="gallery__image"
+                  src="${item.preview}
+                  "data-source="${item.original}"
+                  alt="${item.description}"
+               />
+            </a>
+         </div>`
+      )
+      .join("");
+}
+
+const clickOnImg = (evt) => {
+   evt.preventDefault()
+
+   if (evt.target.nodeName !== "IMG"){
+      return;
+   }
+
+   const createWindowImg = basicLightbox.create(`
+		<img width="1400" height="900" src="${evt.target.dataset.source}" width="1280" height="1024">
+	`);
+
+   createWindowImg.show();
+
+   divGalleryRef.addEventListener("keydown", (evt) => {
+      console.log(evt.key);
+      if (evt.key === "Escape") {
+         createWindowImg.close();
+      }
+   });
+}
+
+divGalleryRef.innerHTML = createGallary(galleryItems);
+
+divGalleryRef.addEventListener("click", clickOnImg);
